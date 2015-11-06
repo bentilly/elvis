d6fc839d2b767688ce70eb820dc86c490503dbd1
Lloyd Phillips <Y981518@B00BEF8.YPG.LOCAL>
2015-11-06 17:15:35 +1300

    Add javascript to toggle dlp-action button states

---
+++ js/core.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+
+    $(".dlp-action").click(function () {
+        //remove all onstate classes
+        $(".dlp-action").removeClass("active");
+        //apply onstate to clicked element
+        $(this).addClass("active");
+    });
+    
+});
