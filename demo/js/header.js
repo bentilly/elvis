a6bc09c17daaecc4d671713df22a66099df67f36
Lloyd Phillips <lloyd.phillips@creationaldesign.com>
2015-12-07 18:07:43 +1300

    Got includes header and footer working for prototype
    Put header and footer into include files so we can reuse across pages

---
+++ demo/js/header.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+
+    //hide/show mobile search
+    $(".search-toggle").on("click", function () {
+        if ($(".toggle-search").hasClass('open')) {
+            $(".toggle-search").removeClass("open");
+        } else {
+            $(".toggle-search").addClass("open");
+        };
+    });
+
+    //hide/show sign in
+    $(".sign-in-button").on("click", function () {
+        $(".sign-in-button").addClass("hide");
+        $(".user-signed-in").removeClass("hide");
+    });
+
+    //hide/show user menu
+    $(".user-signed-in").on("click", function () {
+        if ($(".signed-in-nav").hasClass("open")) {
+            $(".signed-in-nav").removeClass("open");
+            $(".user-signed-in").removeClass("open");
+        } else {
+            $(".signed-in-nav").addClass("open");
+            $(".user-signed-in").addClass("open");
+        };
+    });
+
+    //sign out
+    $("button.sign-out").on("click", function () {
+        $(".signed-in-nav").removeClass("open");
+        $(".user-signed-in").removeClass("open");
+        $(".user-signed-in").addClass("hide");
+        $(".sign-in-button").removeClass("hide");
+    });
+
+});
--- demo/js/includes.js
+++ demo/js/includes.js
@@ -1,9 +1,55 @@
-﻿$(function () {
+﻿$(document).ready(function () {
 
     //the following are includes to assist in common elements for mockup
     //these elements should be removed and templated
 
-    $("#common-footer-include").load("includes/footer.html");
+
+    //### START HEADER
+    $("#common-include-header").load("includes/header.html", function () {
+
+        //hide/show mobile search
+        $(".search-toggle").on("click", function () {
+            if ($(".toggle-search").hasClass('open')) {
+                $(".toggle-search").removeClass("open");
+            } else {
+                $(".toggle-search").addClass("open");
+            };
+        });
+
+        //hide/show sign in
+        $(".sign-in-button").on("click", function () {
+            $(".sign-in-button").addClass("hide");
+            $(".user-signed-in").removeClass("hide");
+        });
+
+        //hide/show user menu
+        $(".user-signed-in").on("click", function () {
+            if ($(".signed-in-nav").hasClass("open")) {
+                $(".signed-in-nav").removeClass("open");
+                $(".user-signed-in").removeClass("open");
+            } else {
+                $(".signed-in-nav").addClass("open");
+                $(".user-signed-in").addClass("open");
+            };
+        });
+
+        //sign out
+        $("button.sign-out").on("click", function () {
+            $(".signed-in-nav").removeClass("open");
+            $(".user-signed-in").removeClass("open");
+            $(".user-signed-in").addClass("hide");
+            $(".sign-in-button").removeClass("hide");
+        });
+
+    });
+    //### END HEADER
+
+
+    //### START FOOTER
+    $("#common-include-footer").load("includes/footer.html");
+    //### END FOOTER
+
+
 
 
     //end include elements
