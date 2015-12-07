261a6943673c4467cae9bc907ecb3ea5e79fa54a
Lloyd Phillips <Y981518@B00BEF8.YPG.LOCAL>
2015-12-07 16:35:57 +1300

    Pre IIS commit
    Commiting files, having to install iis so I can use localhost to use
    includes in jQuery

---
+++ demo/js/includes.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+
+    //the following are includes to assist in common elements for mockup
+    //these elements should be removed and templated
+
+    $("#common-footer-include").load("includes/footer.html");
+
+
+    //end include elements
+    //remove all elements up to here
+
+
+    //$(".dlp-action").click(function () {
+    //    //remove all onstate classes
+    //    $(".dlp-action").removeClass("active");
+    //    //apply onstate to clicked element
+    //    $(this).addClass("active");
+    //});
+
+});
